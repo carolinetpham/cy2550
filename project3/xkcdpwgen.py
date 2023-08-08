8b1874b33e951214bd535e4662d18d49639d9fc3
carolinetpham <carolinetpham@outlook.com>
2023-08-08 19:09:26 +0000

    im literally begging atp

--- project3/xkcdpwgen.py
+++ project3/xkcdpwgen.py
@@ -14,14 +14,20 @@ except getopt.GetoptError:
     sys.exit(2)
 for opt, arg in opts:
     if opt in ("-h", "--help"):
-        print("Usage: xkcdpwgen [-h] [-w WORDS] [-c CAPITALS] [-n NUMBERS] [-s SYMBOLS]")
-        print("Generates a password using the XKCD method.")
-        print("Options:")
-        print("  -h, --help\t\t\tShow this help message and exit.")
-        print("  -w, --words\t\t\tNumber of words in the password. Default is 4.")
-        print("  -c, --capitals\t\tNumber of words to capitalize. Default is 0.")
-        print("  -n, --numbers\t\t\tNumber of numbers to add to the end of the password. Default is 0.")
-        print("  -s, --symbols\t\t\tNumber of symbols to add to the end of the password. Default is 0.")
+        print("usage: xkcdpwgen [-h] [-w WORDS] [-c CAPS] [-n NUMBERS] [-s SYMBOLS]\n\n" +
+              "Generate a secure, memorable password using the XKCD method\n\n" +
+              "Optional args:\n" +
+              "    -h, --help            Show this help message and exit\n" +
+              "    -w WORDS, --words WORDS\n" +
+              "                          Include WORDS words in the password (default=4)\n" +
+              "    -c CAPS, --caps CAPS  Capitalize the first letter of CAPITALS random words\n" +
+              "                          (default=0)\n" +
+              "    -n NUMBERS, --numbers NUMBERS\n" +
+              "                          Insert NUMBERS random numbers in the password\n" +
+              "                          (default=0)\n" +
+              "    -s SYMBOLS, --symbols SYMBOLS\n" +
+              "                          Insert SYMBOLS random symbols in the password\n" +
+              "                          (default=0)")
         sys.exit()
     elif opt in ("-w", "--words"):
         words = int(arg)
