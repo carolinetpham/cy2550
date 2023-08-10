d035f3d2a42bfb5d31954fa9e47d1c2d4a9f0a63
carolinetpham <carolinetpham@outlook.com>
2023-08-10 00:51:52 +0000

    py file

--- project3/xkcdpwgen.py
+++ project3/xkcdpwgen.py
@@ -1,22 +1,22 @@
-#!/usr/bin/env python3
-
 import argparse
 import random
 
+# Generate a password using the XKCD method
 def run(args):
     words = args.words
     caps = args.caps
     numbers = args.numbers
     symbols = args.symbols
-
+    # Read in the word list
     with open('words.txt') as f:
         wordList = [word.strip() for word in f.readlines()]
-
+    # Generate the password
     passwordWords = random.choices(wordList, k=words)
+    # Capitalize random words
     if caps:
         passwordWords = [word.capitalize() for word in passwordWords]
     password = ''.join(passwordWords)
-
+    # Insert numbers and symbols
     if numbers:
         for i in range(numbers):
             position = random.randint(0, len(password))
@@ -28,17 +28,20 @@ def run(args):
             position = random.randint(0, len(password))
             password = password[:position] + random.choice(symbolList) + password[position:]
 
-        return password
+    return password
 
-    def main():
-        parser=argparse.ArgumentParser(description="Generate a secure, memorable password using the XKCD method")
-        parser.add_argument("-w", "--words", help="include WORDS words in the password (default=4)", dest="words", type=int, default=4)
-        parser.add_argument("-c", "--caps", help="capitalize the first letter of CAPS random words (default=0)", dest="caps", type=int, default=0)
-        parser.add_argument("-n", "--numbers", help="insert NUMBERS random numbers in the password (default=0)", dest="numbers", type=int, default=0)
-        parser.add_argument("-s", "--symbols", help="insert SYMBOLS random symbols in the password (default=0)", dest="symbols", type=int, default=0)
-        parser.set_defaults(func=run)
-        args=parser.parse_args()
-        args.func(args)
+# Run the program
+def main():
+    parser = argparse.ArgumentParser(description='Generate a secure, memorable password using the XKCD method')
+    parser.add_argument('-w', '--words', type=int, default=4, help='include WORDS words in the password (default: 4)')
+    parser.add_argument('-c', '--caps', type=int, default=0, help='capitalize the first letter of CAPS random words (default: 0)')
+    parser.add_argument('-n', '--numbers', type=int, default=0, help='insert NUMBERS random numbers in the password (default: 0)')
+    parser.add_argument('-s', '--symbols', type=int, default=0, help='insert SYMBOLS random symbols in the password (default: 0)')
+    args = parser.parse_args()
 
-    if __name__ == '__main__':
-        main()
+    password = run(args)
+    # Print the password
+    print(password)
+
+if __name__ == '__main__':
+    main()
